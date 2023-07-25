1e64f42d852fc26abc9e141c38431e2145af0156
imran-it1 <imranhossainsakil22@gmail.com>
2023-07-25 14:32:21 +0600

    Nav, Banner, Features and Saving Money section added

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Look for any html and js file in the root
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "color-primary": "#01051e",
+        "color-primary-light": "#020726",
+        "color-primary-dark": "#010417",
+        "color-secondary": "#ff7d3b",
+        "color-gray": "#333",
+        "color-white": "#fff",
+        "color-blob": "#A427DF",
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT: '20px',
+          md: "50px"
+        }
+      }
+
+    },
+  },
+  plugins: [],
+}
+
